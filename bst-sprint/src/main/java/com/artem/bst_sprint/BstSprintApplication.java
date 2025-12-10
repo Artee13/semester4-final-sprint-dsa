15512dda66cb53bf67a3236db8524c3888b77e0c
Artee13 <itemalook@gmail.com>
2025-12-10 04:44:15 -0330

    initial spring boot project setup

---
+++ bst-sprint/src/main/java/com/artem/bst_sprint/BstSprintApplication.java
@@ -0,0 +1,13 @@
+package com.artem.bst_sprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BstSprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BstSprintApplication.class, args);
+	}
+
+}
---
+++ bst-sprint/src/test/java/com/artem/bst_sprint/BstSprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artem.bst_sprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BstSprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
